56bfe09c20dc73edb20383759e014b0f5d522647
hugo.allassonniere <hugo.allassonniere@horanet.com>
2026-09-11 16:07:53 +0200

    [MIG] l10n_fr_chorus_facturx: pre-commit auto fixes and adaptation from version 12.0 and version 14.0 and add setup.py

--- l10n_fr_chorus_facturx/models/chorus_flow.py
+++ l10n_fr_chorus_facturx/models/chorus_flow.py
@@ -8,14 +8,14 @@ from odoo import api, fields, models
 class ChorusFlow(models.Model):
     _inherit = 'chorus.flow'
 
-    syntax = fields.Selection(selection_add=[
-        ('xml_cii', 'CII 16B XML'),
-        ('pdf_factur-x', 'Factur-X PDF'),
-        ])
+    syntax = fields.Selection(
+        selection_add=[('xml_cii', "CII 16B XML"), ('pdf_factur-x', "Factur-X PDF")],
+        ondelete={'xml_cii': 'set null', 'pdf_factur-x': "set null"},
+    )
 
     @api.model
     def syntax_odoo2chorus(self):
-        res = super(ChorusFlow, self).syntax_odoo2chorus()
+        res = super().syntax_odoo2chorus()
         res['xml_cii'] = 'IN_DP_E1_CII_16B'
         res['pdf_factur-x'] = 'IN_DP_E2_CII_FACTURX'
         # https://communaute.chorus-pro.gouv.fr/wp-content/uploads/2018/04/AIFE-Chorus-Pro-Qualification-Note-de-livraison-V1.3.3-IT3.pdf
--- l10n_fr_chorus_facturx/models/company.py
+++ l10n_fr_chorus_facturx/models/company.py
@@ -2,17 +2,17 @@
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models, _
+from odoo import _, fields, models
 from odoo.exceptions import ValidationError
 
 
 class ResCompany(models.Model):
     _inherit = 'res.company'
 
-    fr_chorus_invoice_format = fields.Selection(selection_add=[
-        ('xml_cii', 'CII 16B XML'),
-        ('pdf_factur-x', 'Factur-X PDF'),
-        ])
+    fr_chorus_invoice_format = fields.Selection(
+        selection_add=[('xml_cii', "CII 16B XML"), ('pdf_factur-x', "Factur-X PDF")],
+        ondelete={'xml_cii': 'set null', 'pdf_factur-x': 'set null'},
+    )
 
     def _check_chorus_invoice_format(self):
         self.ensure_one()
